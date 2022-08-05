903e87bcf19b125cbde1e432311a7cead7f53936
agvaldezc <agvaldezc@gmail.com>
2022-08-05 16:58:05 -0500

    Added comment to explain PokeAPI service.

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 export const getPokemon = (query) => {
+  // Query can be the name of a pokemon or a number between 1 and 800
   return fetch(`https://pokeapi.co/api/v2/pokemon/${query}`).then((res) => {
     if (res.status === 200) {
       return res.json();
