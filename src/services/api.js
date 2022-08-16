b8bf6f5425a76efa1084993d79e778eb88eded1c
agvaldezc <agvaldezc@gmail.com>
2022-08-16 15:58:48 -0500

    Added API response interface comments.

--- src/services/api.js
+++ src/services/api.js
@@ -12,3 +12,46 @@ export const getPokemon = (query) => {
     }
   });
 };
+
+/*
+interface IBasicData {
+  id: number;
+  name: string;
+}
+
+interface INamedResource {
+  name: string;
+  url: string;
+}
+
+interface IPokemonSprite {
+  front_default: string;
+  front_shiny: string;
+  front_female: string;
+  front_shiny_female: string;
+  back_default: string;
+  back_shiny: string;
+  back_female: string;
+  back_shiny_female: string;
+}
+
+interface IPokemonAbility {
+  ability: INamedResource;
+}
+
+interface IPokemonMove {
+  move: INamedResource;
+}
+
+interface IPokemonType {
+  slot: number;
+  type: INamedResource;
+}
+
+interface IPokemon extends IBasicData {
+  abilities: IPokemonAbility[];
+  moves: IPokemonMove[];
+  types: IPokemonType[];
+  sprites: IPokemonSprite;
+}
+*/
